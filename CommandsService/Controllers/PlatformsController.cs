5716fc29e5c283612c8a2af81e9dde1d8dc8ca0f
faridtb <faridtb@code.edu.az>
2022-07-07 01:22:59 +0400

    feat:Second Service created

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+       public PlatformsController()
+       {
+        
+       }
+
+
+       [HttpPost]
+       public ActionResult TestInboundConnection()
+       {
+           Console.WriteLine("--> Inbound POST # Command Service");
+
+           return Ok("Inbound test of from Platforms Controller");
+       }
+    } 
+}
